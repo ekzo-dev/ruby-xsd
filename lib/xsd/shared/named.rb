8f6ef60aa690e9d868d9f9ef691d4ae1da46fb99
Архипов Дмитрий <da@netcitylife.ru>
2024-04-13 12:42:36 +0200

    Cache namespace calculation

--- lib/xsd/shared/named.rb
+++ lib/xsd/shared/named.rb
@@ -5,8 +5,7 @@ module XSD
     # Get definition namespace
     # @return String
     def namespace
-      is_referenced = respond_to?(:referenced?)
-      is_referenced && referenced? ? reference.schema.target_namespace : schema.target_namespace
+      @namespace ||= (respond_to?(:referenced?) && referenced? ? reference.schema : schema).target_namespace
     end
 
     # Get absolute definition name
