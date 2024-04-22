c3bc6c2fa605e86fe4dddb2a625cf0d8280b31e5
Архипов Дмитрий <da@netcitylife.ru>
2024-04-22 14:54:50 +0200

    Add multiple? shorthand

--- lib/xsd/shared/min_max_occurs.rb
+++ lib/xsd/shared/min_max_occurs.rb
@@ -45,6 +45,12 @@ module XSD
 
     # Determine if element may occur multiple times
     # @return Boolean
+    def multiple?
+      max_occurs == :unbounded || max_occurs > 1
+    end
+
+    # Determine if element may occur multiple times, accounting parents
+    # @return Boolean
     def multiple_allowed?
       computed_max_occurs == :unbounded || computed_max_occurs > 1
     end
